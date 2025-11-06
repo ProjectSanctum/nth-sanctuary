ccecff7a765bc51dc282ff8364b4a2dd93d8c7f8
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-11-05 22:35:29 -0700

    Finally update these maps lol

--- scripts/hooks/Map.lua
+++ scripts/hooks/Map.lua
@@ -10,7 +10,7 @@ end
 
 function Map:onExit()
 	super.onExit(self)
-	if Game.world.map.id and Game.world.map.id ~= "fast_travel" and Game.world.map.id ~= "Map" then
+	if Game.world.map.id and Game.world.map.id ~= "fast_travel" and Game.world.map.id ~= "grand_door" and Game.world.map.id ~= "Map" then
 		Game:setFlag("ft_last_map", Game.world.map.id)
 	end
 end
--- scripts/world/cutscenes/events.lua
+++ scripts/world/cutscenes/events.lua
@@ -59,50 +59,93 @@ return {
         local dd = cutscene:getCharacter("ddelta")
 		local dd_y = dd.y
         cutscene:setSpeaker(dd)
-        cutscene:text("* delta warriors...[wait:5]\n* you finally arrived")
-        cutscene:text("* this is the one and only \"Your Sanctuary\" location")
-        cutscene:text("* very unfortunate timing though cuz it's mine now")
-        cutscene:wait(0.5)
-        cutscene:text("* but i can tell you really want it!!![wait:5]\n* i can tell...")
-        cutscene:text("* so take it from me...")
-        Game.world.music:pause()
-		Assets.playSound("3dprism_appear")
-		local wave_mag = 0
-        local function getFXWaveMag()
-            return wave_mag
-        end
-        dd:addFX(ShaderFX("wave_interlace", {
-            ["wave_sine"] = function () return Kristal.getTime() * 100 end,
-            ["wave_mag"] = function () return getFXWaveMag() end,
-            ["wave_height"] = 2,
-            ["texsize"] = { SCREEN_WIDTH, SCREEN_HEIGHT }
-        }), "funky_mode")
-		Game.world.timer:during(15/30, function()
-			dd.y = MathUtils.lerp(dd.y, dd_y - 40, 0.125)
-			wave_mag = MathUtils.lerp(wave_mag, 120, 0.125)
-		end)
-		cutscene:wait(15/30)
-		wave_mag = 60
-        prism_sprite = Sprite("enemies/3d/idle", dd.x, dd_y)
-		prism_sprite:setLayer(dd.layer)
-		prism_sprite:play(1/30, true)
-        prism_sprite:setOrigin(0.5, 1)
-        prism_sprite:setScale(2, 2)
-        prism_sprite:addFX(ShaderFX("wave_interlace", {
-            ["wave_sine"] = function () return Kristal.getTime() * 100 end,
-            ["wave_mag"] = function () return getFXWaveMag() end,
-            ["wave_height"] = 2,
-            ["texsize"] = { SCREEN_WIDTH, SCREEN_HEIGHT }
-        }), "funky_mode")
-		Game.world:addChild(prism_sprite)
-		dd.visible = false
-		Game.world.timer:during(15/30, function()
-			wave_mag = MathUtils.lerp(wave_mag, 0, 0.25)
-		end)
-		cutscene:wait(15/30)
-		wave_mag = 0
-		cutscene:wait(1)
-        cutscene:text("* IF YOU DARE!!!")
+        if Game:getFlag("prism_quick") ~= true then
+			cutscene:text("* delta warriors...[wait:5]\n* you finally arrived")
+			cutscene:text("* this is the one and only \"Your Sanctuary\" location")
+			cutscene:text("* very unfortunate timing though cuz it's mine now")
+			cutscene:wait(0.5)
+			cutscene:text("* but i can tell you really want it!!![wait:5]\n* i can tell...")
+			cutscene:text("* so take it from me...")
+			Game.world.music:pause()
+			Assets.playSound("3dprism_appear")
+			local wave_mag = 0
+			local function getFXWaveMag()
+				return wave_mag
+			end
+			dd:addFX(ShaderFX("wave_interlace", {
+				["wave_sine"] = function () return Kristal.getTime() * 100 end,
+				["wave_mag"] = function () return getFXWaveMag() end,
+				["wave_height"] = 2,
+				["texsize"] = { SCREEN_WIDTH, SCREEN_HEIGHT }
+			}), "funky_mode")
+			Game.world.timer:during(15/30, function()
+				dd.y = MathUtils.lerp(dd.y, dd_y - 40, 0.125)
+				wave_mag = MathUtils.lerp(wave_mag, 120, 0.125)
+			end)
+			cutscene:wait(15/30)
+			wave_mag = 60
+			prism_sprite = Sprite("enemies/3d/idle", dd.x, dd_y)
+			prism_sprite:setLayer(dd.layer)
+			prism_sprite:play(1/30, true)
+			prism_sprite:setOrigin(0.5, 1)
+			prism_sprite:setScale(2, 2)
+			prism_sprite:addFX(ShaderFX("wave_interlace", {
+				["wave_sine"] = function () return Kristal.getTime() * 100 end,
+				["wave_mag"] = function () return getFXWaveMag() end,
+				["wave_height"] = 2,
+				["texsize"] = { SCREEN_WIDTH, SCREEN_HEIGHT }
+			}), "funky_mode")
+			Game.world:addChild(prism_sprite)
+			dd.visible = false
+			Game.world.timer:during(15/30, function()
+				wave_mag = MathUtils.lerp(wave_mag, 0, 0.25)
+			end)
+			cutscene:wait(15/30)
+			wave_mag = 0
+			cutscene:wait(1)
+			cutscene:text("* IF YOU DARE!!!")
+            Game:setFlag("prism_quick", true)
+			Game:saveQuick()
+		else
+			cutscene:text("* let's just cut to the chase.")
+			Game.world.music:pause()
+			Assets.playSound("3dprism_appear")
+			local wave_mag = 0
+			local function getFXWaveMag()
+				return wave_mag
+			end
+			dd:addFX(ShaderFX("wave_interlace", {
+				["wave_sine"] = function () return Kristal.getTime() * 100 end,
+				["wave_mag"] = function () return getFXWaveMag() end,
+				["wave_height"] = 2,
+				["texsize"] = { SCREEN_WIDTH, SCREEN_HEIGHT }
+			}), "funky_mode")
+			Game.world.timer:during(15/30, function()
+				dd.y = MathUtils.lerp(dd.y, dd_y - 40, 0.125)
+				wave_mag = MathUtils.lerp(wave_mag, 120, 0.125)
+			end)
+			cutscene:wait(15/30)
+			wave_mag = 60
+			prism_sprite = Sprite("enemies/3d/idle", dd.x, dd_y)
+			prism_sprite:setLayer(dd.layer)
+			prism_sprite:play(1/30, true)
+			prism_sprite:setOrigin(0.5, 1)
+			prism_sprite:setScale(2, 2)
+			prism_sprite:addFX(ShaderFX("wave_interlace", {
+				["wave_sine"] = function () return Kristal.getTime() * 100 end,
+				["wave_mag"] = function () return getFXWaveMag() end,
+				["wave_height"] = 2,
+				["texsize"] = { SCREEN_WIDTH, SCREEN_HEIGHT }
+			}), "funky_mode")
+			Game.world:addChild(prism_sprite)
+			dd.visible = false
+			Game.world.timer:during(15/30, function()
+				wave_mag = MathUtils.lerp(wave_mag, 0, 0.25)
+			end)
+			cutscene:wait(15/30)
+			wave_mag = 0
+			cutscene:wait(1)
+		end
         cutscene:startEncounter("3d")
 		prism_sprite:remove()
 		dd.y = dd_y
