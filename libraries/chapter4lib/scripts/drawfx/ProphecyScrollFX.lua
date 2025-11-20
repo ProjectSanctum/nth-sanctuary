685bfaf3c5ef16a959f625c5f05d5e12ac2b10ed
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-11-19 21:17:49 -0700

    Update ProphecyScrollFX.lua

--- libraries/chapter4lib/scripts/drawfx/ProphecyScrollFX.lua
+++ libraries/chapter4lib/scripts/drawfx/ProphecyScrollFX.lua
@@ -73,7 +73,6 @@ function ProphecyScrollFX:drawPart(texture, min, max, alpha)
     local pnl_canvas = Draw.pushCanvas(pnl_tex:getDimensions())
     draw_sprite_tiled_ext(pnl_tex, 0, 0, 0, 1, 1, oldHexToRgb("#42D0FF", alpha))
     Draw.popCanvas(true)
-    self.tick = self.tick + (((1/15) * self.scroll_speed) * DTMULT);
     local x, y = -((_cx * 2) + (self.tick * 15)) * 0.5, -((_cy * 2) + (self.tick * 15)) * 0.5
     draw_sprite_tiled_ext(Assets.getTexture("backgrounds/IMAGE_DEPTH_EXTEND_MONO_SEAMLESS_BRIGHTER"), 0, x, y, 2, 2, oldHexToRgb("#42D0FF", 1));
     local orig_bm, orig_am = love.graphics.getBlendMode()
