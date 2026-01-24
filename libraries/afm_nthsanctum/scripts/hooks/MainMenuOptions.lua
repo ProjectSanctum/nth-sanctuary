82906ca9c109e82cd733d6a3021fd6907f49e000
Ralszor <nemalock@gmail.com>
2026-01-24 16:30:48 -0500

    h

--- libraries/afm_nthsanctum/scripts/hooks/MainMenuOptions.lua
+++ libraries/afm_nthsanctum/scripts/hooks/MainMenuOptions.lua
@@ -8,6 +8,10 @@ end
 function MainMenuOptions:initializeOptions()
     super.initializeOptions(self)
     self:registerConfigOption({"graphics" }, "Remove Hue Shift", "nthSanctuary/removeHSV")
+    self:registerConfigOption({"graphics" }, "potat mode :)", "nthSanctuary/removeHSV", function (toggled)
+        self.menu.mod_list:buildModList()
+    end)
+    
 end
 
 
