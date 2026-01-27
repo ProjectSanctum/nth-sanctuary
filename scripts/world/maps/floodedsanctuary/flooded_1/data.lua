36387694b083146bc59edd8843c5d9a2c1e477b9
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-01-26 20:46:17 -0800

    Fixed room transition mismatch

--- scripts/world/maps/floodedsanctuary/flooded_1/data.lua
+++ scripts/world/maps/floodedsanctuary/flooded_1/data.lua
@@ -1,7 +1,7 @@
 return {
   version = "1.10",
   luaversion = "5.1",
-  tiledversion = "1.10.2",
+  tiledversion = "1.11.2",
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
@@ -985,7 +985,7 @@ return {
           rotation = 0,
           visible = true,
           properties = {
-            ["map"] = "floodedsanctuary/hellentrance",
+            ["map"] = "floodedsanctuary/between",
             ["marker"] = "entry4"
           }
         }
