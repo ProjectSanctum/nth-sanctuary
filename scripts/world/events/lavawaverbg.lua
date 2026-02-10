5137444ec02b23f8519ffbce7c3456f81ed9ab8f
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-02-09 17:06:00 -0800

    Cumber

--- scripts/world/events/lavawaverbg.lua
+++ scripts/world/events/lavawaverbg.lua
@@ -3,7 +3,6 @@ local LavaWaverBG, super = Class(Event)
 function LavaWaverBG:init(data)
     super.init(self, data)
     local properties = data and data.properties or {}
-
     self.debug_select = false
     self.bg_tex = Assets.getTexture(properties["texture"] or "world/events/lavabg")
 end
@@ -11,17 +10,13 @@ end
 ---@see Draw.drawWrapped
 local function drawWrappedHack(drawable, wrap_w, wrap_h)
     local dw, dh = drawable:getDimensions()
-
     local wrap_area_l, wrap_area_u = love.graphics.inverseTransformPoint(0, 0)
     -- changed from SCREEN_WIDTH etc.
     local wrap_area_r, wrap_area_d = love.graphics.inverseTransformPoint(wrap_w, wrap_h)
-
     local x1, y1 = math.min(wrap_area_l, wrap_area_r), math.min(wrap_area_u, wrap_area_d)
     local x2, y2 = math.max(wrap_area_l, wrap_area_r), math.max(wrap_area_u, wrap_area_d)
-
     local x_offset = math.floor(x1 / dw) * dw
     local y_offset = math.floor(y1 / dh) * dh
-
     local wrap_width = math.ceil((x2 - x_offset) / dw)
     local wrap_height = math.ceil((y2 - y_offset) / dh)
 
@@ -35,16 +30,13 @@ end
 function LavaWaverBG:draw()
     if self.world.color ~= COLORS.black then
         love.graphics.push()
-
         local tw, th = self.bg_tex:getWidth() * self.scale_x, self.bg_tex:getHeight() * self.scale_y
-
         local scratchpad = Draw.pushCanvas(self.world.width + tw*2, self.world.height + th*2)
         love.graphics.translate(tw, th)
         Draw.setColor(1, 1, 1, 0.80 + (self.world.map.lava_alpha or 0))
         drawWrappedHack(self.bg_tex, scratchpad:getWidth(), scratchpad:getHeight())
         Draw.setColor(1, 1, 1, 1)
         Draw.popCanvas()
-
         love.graphics.translate(-self.x, -self.y) -- undo object position
         Draw.pushShader("lavawave", {
             wave_sine = Kristal.getTime() * 90,
@@ -54,7 +46,6 @@ function LavaWaverBG:draw()
         })
         Draw.draw(scratchpad, -tw, -th)
         Draw.popShader()
-
         love.graphics.pop()
     end
 
