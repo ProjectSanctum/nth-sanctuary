65960464cdf766de728ee7de6a5f392fb204fdd8
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-02-25 01:40:37 -0700

    a

---
+++ scripts/world/tilesets/RandomExtraTiles.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.2",
+  name = "RandomExtraTiles",
+  class = "",
+  tilewidth = 20,
+  tileheight = 20,
+  spacing = 0,
+  margin = 0,
+  columns = 6,
+  image = "../../../assets/sprites/tilesets/RandomExtraTiles.png",
+  imagewidth = 120,
+  imageheight = 960,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 20,
+    height = 20
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 288,
+  tiles = {}
+}
