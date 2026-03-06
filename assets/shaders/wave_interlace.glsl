5be59d37d6c1b1b82df5070b4ea0c94f78dd9ea5
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-03-06 13:15:21 -0700

    Fix wave_interlace shader

--- assets/shaders/wave_interlace.glsl
+++ assets/shaders/wave_interlace.glsl
@@ -4,8 +4,8 @@ uniform float wave_height;
 uniform vec2 texsize;
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
-	float i = floor((texture_coords.y * texsize.y)/2)*2;
-	if ((mod(i, 4) >= 2)) {
+	float i = floor((texture_coords.y * texsize.y)/2.0)*2.0;
+	if ((mod(i, 4.0) >= 2.0)) {
 		vec2 coords = vec2(max(0.0, min(1.0, texture_coords.x - (sin((i / wave_height) + (wave_sine / 30.0)) * wave_mag) / texsize.x)), max(0.0, min(1.0, texture_coords.y + 0.0)));
 		return Texel(texture, coords) * color;
 	}
