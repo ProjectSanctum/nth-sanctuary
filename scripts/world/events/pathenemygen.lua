e0ed5200013b5c367ea7b40454b2c6ee6df08351
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-03-05 19:55:31 -0800

    Fucking bullet that I hate

--- scripts/world/events/pathenemygen.lua
+++ scripts/world/events/pathenemygen.lua
@@ -3,6 +3,7 @@ local PathingEnemyGenerator, super = Class(Event, "pathenemygen")
 function PathingEnemyGenerator:init(data)
     super.init(self, data)
     local properties = data and data.properties or {}
+	self.spawnamount = properties["spawnamount"] or false
 	self.timer = properties["timer"] or 0
 	self.rate = properties["rate"] or 0
 	self.damage = properties["damage"] or 25
@@ -21,6 +22,70 @@ function PathingEnemyGenerator:init(data)
     self.progress = (properties["progress"] or 0) % 1
 end
 
+function PathingEnemyGenerator:onAdd(parent)
+    super.onAdd(self, parent)
+
+    self:snapToPath()
+end
+
+function PathingEnemyGenerator	:snapToPath()
+    if self.path and self.world.map.paths[self.path] then
+        local path = self.world.map.paths[self.path]
+
+        local progress = self.progress
+        if not path.closed then
+            progress = Ease.inOutSine(progress, 0, 1, 1)
+        end
+
+        if path.shape == "line" then
+            local dist = progress * path.length
+            local current_dist = 0
+
+            for i = 1, #path.points - 1 do
+                local next_dist = MathUtils.dist(path.points[i].x, path.points[i].y, path.points[i + 1].x, path.points[i + 1].y)
+
+                if current_dist + next_dist > dist then
+                    local x = MathUtils.lerp(path.points[i].x, path.points[i + 1].x, MathUtils.clamp((dist - current_dist) / next_dist, 0, 1))
+                    local y = MathUtils.lerp(path.points[i].y, path.points[i + 1].y, MathUtils.clamp((dist - current_dist) / next_dist, 0, 1))
+
+                    if self.debug_x and self.debug_y and Kristal.DebugSystem.last_object == self then
+                        x = Utils.ease(self.debug_x, x, Kristal.DebugSystem.release_timer, "outCubic")
+                        y = Utils.ease(self.debug_y, y, Kristal.DebugSystem.release_timer, "outCubic")
+                        if Kristal.DebugSystem.release_timer >= 1 then
+                            self.debug_x = nil
+                            self.debug_y = nil
+                        end
+                    end
+
+                    self.x = x
+					self.y = y
+					self.sprite.rotation = MathUtils.angle(path.points[i].x, path.points[i].y, path.points[i + 1].x, path.points[i + 1].y) - math.rad(90)
+                    break
+                else
+                    current_dist = current_dist + next_dist
+                end
+            end
+        elseif path.shape == "ellipse" then
+            local angle = progress * (math.pi * 2)
+            local x = path.x + math.cos(angle) * path.rx
+            local y = path.y + math.sin(angle) * path.ry
+
+            if self.debug_x and self.debug_y and Kristal.DebugSystem.last_object == self then
+                x = Utils.ease(self.debug_x, x, Kristal.DebugSystem.release_timer, "outCubic")
+                y = Utils.ease(self.debug_y, y, Kristal.DebugSystem.release_timer, "outCubic")
+                if Kristal.DebugSystem.release_timer >= 1 then
+                    self.debug_x = nil
+                    self.debug_y = nil
+                end
+            end
+
+            self.x = x
+			self.y = y
+			self.sprite.rotation = angle
+        end
+    end
+end
+
 function PathingEnemyGenerator:getDebugRectangle()
     return {-5, -5, 10, 10}
 end
@@ -32,7 +97,8 @@ function PathingEnemyGenerator:update()
 		return
 	end
 	self.timer = self.timer + DTMULT
-	if self.timer >= self.rate then
+	if self.spawnamount > 0 then
+		if self.timer >= self.rate then
 		local bullet = self.world:spawnBullet("pathingenemy", self.x, self.y, {
 			damage = self.damage,
 			movetype = self.movetype,
@@ -52,6 +118,8 @@ function PathingEnemyGenerator:update()
 		bullet:setLayer(self.layer)
 		self.timer = 0
 	end
+	end
+	
 end
 
 return PathingEnemyGenerator
