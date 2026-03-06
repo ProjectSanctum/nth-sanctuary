2a815ab733063ba5ea3e0dfae13848318676a258
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-03-06 00:42:03 -0700

    Fix rotating tower reticle drawing layer
    This was done by moving it to its own object.

--- libraries/chapter4lib/scripts/world/events/rotatingtower.lua
+++ libraries/chapter4lib/scripts/world/events/rotatingtower.lua
@@ -89,6 +89,9 @@ function RotatingTower:init(data)
 	Game.world.map.cyltower = self
 	self.x = 0
 	self.y = 0
+	self.reticle_drawer = RotatingTowerReticleDrawer(self)
+	self.reticle_drawer.layer = Game.world.player and Game.world.player.layer or self.layer
+	Game.world:addChild(self.reticle_drawer)
 end
 
 function RotatingTower:getTileIndex(x,y)
@@ -297,7 +300,6 @@ function RotatingTower:draw()
 			end
 		end
 	end
-	self:drawClimbReticle()
 	local cull_top = render_ypos_start * self.tile_height_fine
 	local cull_bottom = render_ypos_end * self.tile_height_fine
 	if self.appearance == 0 then
@@ -310,181 +312,4 @@ function RotatingTower:draw()
 	Draw.setColor(1,1,1,1)
 end
 
-function RotatingTower:drawClimbReticle()
-    local player = self.world.player
-    if not player.draw_reticle or player.state ~= "CLIMB" then
-        return
-    end
-	love.graphics.push()
-    --love.graphics.translate(player.width/2, player.height - 10)
-
-    local found = 0;
-    local _alph;
-
-    if (player.jumpchargecon ~= 0) then
-        local count = 1;
-
-        for i = 1, #player.charge_times do
-            if player.jumpchargetimer > player.charge_times[i] then
-                count = i + 1
-            end
-        end
-
-        local px = 0;
-        local py = 0;
-
-        for i = 1, count do
-            if (player.facing == "down") then
-                py = 0+i;
-            end
-
-            if (player.facing == "right") then
-                px = 0+i;
-            end
-
-            if (player.facing == "up") then
-                py = 0-i;
-            end
-
-            if (player.facing == "left") then
-                px = 0-i;
-            end
-            local s,o = player:canClimb(px, py)
-            if s or o then
-                found = i
-            end
-        end
-		local px, py = self.world.player:getRelativePos(0, 20)
-		if self.appearance == 1 then
-			px = px + 40
-		end
-		local tilex = px / self.tile_width_fine
-		local tiley = py / self.tile_height_fine
-		if tilex >= self.horizontaltilecount then
-			tilex = tilex - self.horizontaltilecount
-		end
-		if tilex < 0 then
-			tilex = tilex + self.horizontaltilecount
-		end
-        _alph = MathUtils.clamp(player.jumpchargetimer / 14, 0.1, 0.8);
-        local angle = 0;
-        local xoff = 0;
-        local yoff = 0;
-		local shiftx = 0
-		local shifty = 0
-
-        if (player.facing == "down") then
-            angle = 0;
-            xoff = -22;
-            yoff = 18;
-			shifty = 1
-        end
-
-        if (player.facing == "right") then
-            angle = 90;
-            xoff = 18;
-            yoff = 22;
-			shiftx = 1
-        end
-
-        if (player.facing == "up") then
-            angle = 180;
-            xoff = 22;
-            yoff = -18;
-			shifty = -1
-        end
-
-        if (player.facing == "left") then
-            angle = 270;
-            xoff = -18;
-            yoff = -22;
-			shiftx = -1
-        end
-
-        -- TODO: Put these colors in the PALETTE
-        local col = {200/255, 200/255, 200/255};
-
-        if (found ~= 0) then
-            col = {255/255, 200/255, 132/255};
-        end
-        Draw.setColor(col)
-        local frame = MathUtils.wrap(math.floor(Kristal.getTime() * 15), 1,4)
-        local w = (player.jumpchargetimer / (player.charge_times[#player.charge_times] or 10)) * (#player.charge_times+1)
-		local tile = self.tile_data[self.tm_tileset[1]][math.floor(tilex) + 1]
-		local startx = (self.tower_x + tile.x + xoff) - 20
-		local starty = py + yoff + 20
-		local divisor = 120
-		local count = #player.charge_times
-        for subsection = 0, count do
-            local id, h = "ui/climb/hint_mid", 20
-            if subsection == 0 then
-                id = "ui/climb/hint_start"
-                h = 21
-            elseif subsection == #player.charge_times then
-                id = "ui/climb/hint_end"
-                h = 21
-            end
-			local tx = tilex + ((subsection + 1) * shiftx)
-			local ty = tiley + ((subsection + 1) * shifty)
-			if tx >= self.horizontaltilecount then
-				tx = tx - self.horizontaltilecount
-			end
-			if tx < 0 then
-				tx = tx + self.horizontaltilecount
-			end
-			local tile2 = self.tile_data[self.tm_tileset[1]][math.floor(tx) + 1]
-			if tile2.vis == 1 then
-				local jankfix = 0
-				if subsection == (#player.charge_times - 1) and self.tile_width_fine ~= self.tile_width and shiftx == -1 then
-					jankfix = (6 * (shiftx - 1)) / 2
-				end
-				local scalemult = tile2.xscale / self.tile_width_fine
-				local quad = Assets.getQuad(0, 0, 22, math.floor(MathUtils.clamp(w - subsection, 0, 1) * h), 22, h)
-				Draw.draw(Assets.getFrames(id)[frame], quad, startx - jankfix, starty, -math.rad(angle), 2, scalemult * -2)
-				startx = startx + (scalemult * shiftx * h * -2)
-				starty = starty + (shifty * h * 2)
-			end
-		end
-    end
-	
-    if (player.jumpchargecon > 0 and found ~= 0) then
-		local px, py = self.world.player:getRelativePos(0, 20)
-		
-		if self.appearance == 1 then
-			px = px + 40
-		end
-
-        if (player.facing == "down") then
-            py = py + (self.tile_height * found);
-        end
-
-        if (player.facing == "right") then
-            px = px + (self.tile_width * found);
-        end
-
-        if (player.facing == "up") then
-            py = py - (self.tile_height * found);
-        end
-
-        if (player.facing == "left") then
-            px = px - (self.tile_width * found);
-        end
-		
-		local tilex = px / self.tile_width_fine
-		if tilex >= self.horizontaltilecount then
-			tilex = tilex - self.horizontaltilecount
-		end
-		if tilex < 0 then
-			tilex = tilex + self.horizontaltilecount
-		end
-
-        Draw.setColor(TableUtils.lerp({1,1,0,_alph}, {1,1,1,_alph}, 0.4 + (math.sin(player.jumpchargetimer / 3) * 0.4)));
-		local tile = self.tile_data[self.tm_tileset[1]][math.floor(tilex) + 1]
-		if tile.vis == 1 then
-			Draw.draw(Assets.getTexture("ui/climb/reticle"), self.tower_x + tile.x, py, 0, (tile.xscale / self.tile_width_fine) * 2, 2, 2, 2)
-		end
-	end
-    love.graphics.pop()
-	Draw.setColor(1,1,1,1)
-end
 return RotatingTower
