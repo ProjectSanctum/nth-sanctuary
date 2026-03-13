4c1b55fefc3bbec231be7964e4b9aadd1ded192f
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-03-12 17:47:35 -0700

    Hacky fix for ProphecyShaderFX positioning

--- libraries/chapter4lib/scripts/drawfx/ProphecyShaderFX.lua
+++ libraries/chapter4lib/scripts/drawfx/ProphecyShaderFX.lua
@@ -10,14 +10,24 @@ function ProphecyShaderFX:init(opacity, color, priority)
     self.shader:send("sampler_2", self.texture_2)
 	self.opacity = opacity or 1
 	self.col = color or {0.2588, 0.8157, 1}
+    self.transformed = false
 end
 
 function ProphecyShaderFX:draw(texture)
-    --Draw.drawCanvas(texture)
+    local last_shader = love.graphics.getShader()
+    love.graphics.setShader(self.shader)
     self.shader:send("time", Kristal.getTime() * 15)
     self.shader:send("opacity", self.opacity)
+	if self.parent.parallax_x or self.parent.parallax_y then
+		self.shader:send("camx", -Game.world.camera.x*self.parent.parallax_x/2)
+		self.shader:send("camy", -Game.world.camera.y*self.parent.parallax_y/2)
+	else
+		self.shader:send("camx", -Game.world.camera.x/2)
+		self.shader:send("camy", -Game.world.camera.y/2)
+	end		
     self.shader:sendColor("col", self.col)
-    super.draw(self, texture)
+    Draw.drawCanvas(texture)
+    love.graphics.setShader(last_shader)
 end
 
 return ProphecyShaderFX
