1837ffc538ba733acf23dbfc1003a96837691b2e
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-03-14 08:30:41 -0700

    You forgot one lucid

--- scripts/world/maps/2_2nd_sanctuary/second_sanctum_4_crossroad/data.lua
+++ scripts/world/maps/2_2nd_sanctuary/second_sanctum_4_crossroad/data.lua
@@ -1,7 +1,7 @@
 return {
   version = "1.10",
   luaversion = "5.1",
-  tiledversion = "1.11.2",
+  tiledversion = "1.10.2",
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
@@ -131,7 +131,9 @@ return {
           rotation = 0,
           gid = 1247,
           visible = true,
-          properties = {}
+          properties = {
+            ["color"] = "#ff144139"
+          }
         }
       }
     },
