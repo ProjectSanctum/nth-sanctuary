ee997b5e48c32f402c8779985d72f93e0ccc87a5
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-03-23 03:48:44 -0700

    Too many bookshelves!

--- scripts/world/tilesets/bg_dw_church_library_2_tileset.tsx
+++ scripts/world/tilesets/bg_dw_church_library_2_tileset.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="bg_dw_church_library_2_tileset" tilewidth="40" tileheight="40" tilecount="260" columns="10">
+<tileset version="1.10" tiledversion="1.11.2" name="bg_dw_church_library_2_tileset" tilewidth="40" tileheight="40" tilecount="260" columns="10">
  <image source="../../../assets/sprites/tilesets/dr ch3-4 fixed tilesets/bg_dw_church_library_2_tileset.png" width="400" height="1040"/>
+ <tile id="161" probability="0.1"/>
+ <tile id="187" probability="0.1"/>
+ <tile id="192" probability="0.1"/>
+ <tile id="218" probability="0.1"/>
 </tileset>
