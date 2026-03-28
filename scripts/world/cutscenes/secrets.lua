367876db8e6c1f88e9ce7a218b007ff92f6b06b7
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-03-27 19:49:24 -0700

    Check engine light

--- scripts/world/cutscenes/secrets.lua
+++ scripts/world/cutscenes/secrets.lua
@@ -3,7 +3,7 @@ return {
         if Game:getFlag("fun") ~= 69 then
             cutscene:text("* (Holy shit it's Frisk from the game UNDERTALE.)")
         else
-            cutscene:text("* (It's some sort of [image:npcs/frisk/engine, 0,0,2,2].)")
+            cutscene:text("* (It's some sort of [image:npcs/frisk/engine, -5,10,1.5,1.5].)")
         end
     end,
     proph = function (cutscene)
