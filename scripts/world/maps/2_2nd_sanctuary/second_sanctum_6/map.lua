30b98aafe6a9080210be537b8d5b8e141e665450
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-04-13 07:53:42 -0700

    Update map.lua

--- scripts/world/maps/2_2nd_sanctuary/second_sanctum_6/map.lua
+++ scripts/world/maps/2_2nd_sanctuary/second_sanctum_6/map.lua
@@ -1,9 +1,13 @@
 ---@class Map.dark_place : Map
 local map, super = Class(Map, "second_sanctum_6")
 
-function map:onEnter()
+function map:init(world, data)
+	super.init(self, world, data)
 	self.bookshelf_pos = {}
 	self.bookshelf_debris_sprites = {}
+end
+
+function map:onEnter()
 	for _, shelf in ipairs(Game.world.map:getEvents("bookshelf_destructable")) do
 		if shelf then
 			table.insert(self.bookshelf_pos, {x = shelf.x, y = shelf.y, layer = shelf.layer, width = shelf.width, height = shelf.height, uid = shelf.unique_id, oid = shelf.object_id, sprite = shelf.texture, setflag = shelf.setflag, setvalue = shelf.setvalue, once = shelf.once})
