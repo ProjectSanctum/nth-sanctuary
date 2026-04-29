8c17fa67ac946186e27b8aaa026dbc4132b9cdff
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-04-29 14:50:53 -0700

    Better Wall Arches Tileset made

---
+++ scripts/world/tilesets/ExtraWallArchTiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.1" name="ExtraWallArchTiles" tilewidth="40" tileheight="40" tilecount="54" columns="6">
+ <editorsettings>
+  <export target="ExtraWallArchTiles.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/ExtraWallArchTiles.png" width="240" height="360"/>
+</tileset>
