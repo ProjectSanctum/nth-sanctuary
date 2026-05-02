562ee4ee2c2678d4961f97e678a600e9ca6d82d5
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-01 21:51:07 -0700

    Fractured battle HSV now only affects BG, party and enemies

--- scripts/hooks/Encounter.lua
+++ scripts/hooks/Encounter.lua
@@ -1,17 +1,27 @@
 local Encounter, super = HookSystem.hookScript(Encounter)
 
-
 function Encounter:init()
     super.init(self)
+	self.frac_hsv_fx = FracturedHSVFX()
+end
+
+function Encounter:onBattleInit()
     if StringUtils.contains(Game.world.map.id, "fractured") then
-        
-        Game.battle:addFX(FracturedHSVFX())
-    end
+		for _, battler in ipairs(Game.battle.party) do
+			battler:addFX(self.frac_hsv_fx)
+		end
+		for _, battler in ipairs(Game.battle.enemies) do
+			battler:addFX(self.frac_hsv_fx)
+		end
+	end
+    super.onBattleInit(self)
 end
 
 function Encounter:createBackground()
     if StringUtils.contains(Game.world.map.id, "fractured") then
-        return Game.battle:addChild(FracturedBattleBG())
+		local frac_bg = FracturedBattleBG()
+        frac_bg:addFX(self.frac_hsv_fx)
+        return Game.battle:addChild(frac_bg)
     end
     return super.createBackground(self)
 end
