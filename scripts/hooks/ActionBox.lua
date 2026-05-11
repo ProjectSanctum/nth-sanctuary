cfb7863757ec1c21cf923f023a96a98a0fb96e0d
ddeltatemp <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-11 16:30:33 -0700

    Lobby Man party member static effect

---
+++ scripts/hooks/ActionBox.lua
@@ -0,0 +1,65 @@
+local ActionBox, super = HookSystem.hookScript(ActionBox)
+
+function ActionBox:init(x, y, index, battler)
+    super.init(self, x, y, index, battler)
+
+	if battler.chara.id == "lobbyman_party" then
+		local static_fx = ShaderFX(Mod.staticBulletShader, {
+			["time"] = function() return Kristal.getTime() end,
+			["brightness"] = 0.5
+		})
+		self.head_sprite:addFX(static_fx, "static_fx")
+	end
+end
+
+function ActionBox:drawActionBox()
+    if Game.battle.current_selecting == self.index then
+		if self.battler.chara.id == "lobbyman_party" then
+            Draw.setColor(COLORS.white)
+			local static_shader = Mod.staticBulletShader
+			static_shader:send("time", Kristal.getTime())
+			static_shader:send("brightness", 0.5)
+            love.graphics.setShader(static_shader)
+		end
+        Draw.setColor(self.battler.chara:getColor())
+        love.graphics.setLineWidth(2)
+        love.graphics.line(1  , 2, 1,   37)
+        love.graphics.line(Game:getConfig("oldUIPositions") and 211 or 212, 2, Game:getConfig("oldUIPositions") and 211 or 212, 37)
+        love.graphics.line(0  , 6, 212, 6 )
+        love.graphics.setShader()
+    end
+    Draw.setColor(1, 1, 1, 1)
+end
+
+function ActionBox:drawSelectionMatrix()
+    -- Draw the background of the selection matrix
+    Draw.setColor(0, 0, 0, 1)
+    love.graphics.rectangle("fill", 2, 2, 209, 35)
+
+    if Game.battle.current_selecting == self.index then
+        local r,g,b,a = self.battler.chara:getColor()
+		if self.battler.chara.id == "lobbyman_party" then
+            r,g,b,a = 1,1,1,1
+			local static_shader = Mod.staticBulletShader
+			static_shader:send("time", Kristal.getTime())
+			static_shader:send("brightness", 0.5)
+            love.graphics.setShader(static_shader)
+		end
+		
+        for i = 0, 11 do
+            local siner = self.selection_siner + (i * (10 * math.pi))
+
+            love.graphics.setLineWidth(2)
+            Draw.setColor(r, g, b, a * math.sin(siner / 60))
+            if math.cos(siner / 60) < 0 then
+                love.graphics.line(1 - (math.sin(siner / 60) * 30) + 30, 0, 1 - (math.sin(siner / 60) * 30) + 30, 37)
+                love.graphics.line(211 + (math.sin(siner / 60) * 30) - 30, 0, 211 + (math.sin(siner / 60) * 30) - 30, 37)
+            end
+        end
+        love.graphics.setShader()
+
+        Draw.setColor(1, 1, 1, 1)
+    end
+end
+
+return ActionBox
---
+++ scripts/hooks/OverworldActionBox.lua
@@ -0,0 +1,114 @@
+local OverworldActionBox, super = HookSystem.hookScript(OverworldActionBox)
+
+function OverworldActionBox:init(x, y, index, chara)
+    super.init(self, x, y, index, chara)
+	
+	if chara.id == "lobbyman_party" then
+		local static_fx = ShaderFX(Mod.staticBulletShader, {
+			["time"] = function() return Kristal.getTime() end,
+			["brightness"] = 0.5
+		})
+		self.head_sprite:addFX(static_fx, "static_fx")
+	end
+end
+
+function OverworldActionBox:draw()
+    -- Draw the line at the top
+	local health_bg_col = PALETTE["action_health_bg"]
+    if self.selected then
+        Draw.setColor(self.chara:getColor())
+		if self.chara.id == "lobbyman_party" then
+			health_bg_col = COLORS.dkgray
+            Draw.setColor(COLORS.white)
+			local static_shader = Mod.staticBulletShader
+			static_shader:send("time", Kristal.getTime())
+			static_shader:send("brightness", 0.5)
+            love.graphics.setShader(static_shader)
+		end
+    else
+        Draw.setColor(PALETTE["action_strip"])
+		if self.chara.id == "lobbyman_party" then
+			health_bg_col = COLORS.dkgray
+		end
+    end
+
+    love.graphics.setLineWidth(2)
+    love.graphics.line(0, 1, 213, 1)
+    
+    if Game:getConfig("oldUIPositions") then
+        love.graphics.line(0, 2, 2, 2)
+        love.graphics.line(211, 2, 213, 2)
+    end
+    love.graphics.setShader()
+
+    -- Draw health
+    Draw.setColor(health_bg_col)
+    love.graphics.rectangle("fill", 128, 24, 76, 9)
+
+    local health = (self.chara:getHealth() / self.chara:getStat("health")) * 76
+
+    if health > 0 then
+        Draw.setColor(self.chara:getColor())
+		if self.chara.id == "lobbyman_party" then
+            Draw.setColor(COLORS.white)
+			local static_shader = Mod.staticBulletShader
+			static_shader:send("time", Kristal.getTime())
+			static_shader:send("brightness", 1)
+            love.graphics.setShader(static_shader)
+		end
+        love.graphics.rectangle("fill", 128, 24, math.ceil(health), 9)
+    end
+    love.graphics.setShader()
+
+    local color = PALETTE["action_health_text"]
+    if health <= 0 then
+        color = PALETTE["action_health_text_down"]
+    elseif (self.chara:getHealth() <= (self.chara:getStat("health") / 4)) then
+        color = PALETTE["action_health_text_low"]
+    else
+        color = PALETTE["action_health_text"]
+    end
+
+    local health_offset = 0
+    health_offset = (#tostring(self.chara:getHealth()) - 1) * 8
+
+    Draw.setColor(color)
+    love.graphics.setFont(self.font)
+    love.graphics.print(self.chara:getHealth(), 152 - health_offset, 11)
+    Draw.setColor(PALETTE["action_health_text"])
+    love.graphics.print("/", 161, 11)
+    local string_width = self.font:getWidth(tostring(self.chara:getStat("health")))
+    Draw.setColor(color)
+    love.graphics.print(self.chara:getStat("health"), 205 - string_width, 11)
+
+    -- Draw name text if there's no sprite
+    if not self.name_sprite then
+        local font = Assets.getFont("name")
+        love.graphics.setFont(font)
+        Draw.setColor(1, 1, 1, 1)
+
+        local name = self.chara:getName():upper()
+        local spacing = 5 - StringUtils.len(name)
+
+        local off = 0
+        for i = 1, StringUtils.len(name) do
+            local letter = StringUtils.sub(name, i, i)
+            love.graphics.print(letter, 51 + off, 16 - 1)
+            off = off + font:getWidth(letter) + spacing
+        end
+    end
+
+    local reaction_x = -1
+
+    if self.x == 0 then -- lazy check for leftmost party member
+        reaction_x = 3
+    end
+
+    love.graphics.setFont(self.main_font)
+    Draw.setColor(1, 1, 1, self.reaction_alpha / 6)
+    love.graphics.print(self.reaction_text, reaction_x, 43, 0, 0.5, 0.5)
+
+    super.super.draw(self)
+end
+
+return OverworldActionBox
