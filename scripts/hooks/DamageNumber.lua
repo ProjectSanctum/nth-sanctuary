2593226a3392f72bde16e9463e26267de743b932
ddeltatemp <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-11 17:25:06 -0700

    c

--- scripts/hooks/DamageNumber.lua
+++ scripts/hooks/DamageNumber.lua
@@ -6,7 +6,7 @@ function DamageNumber:init(type, arg, x, y, color, delay, chara)
 	if chara and chara.id == "lobbyman_party" and type == "damage" then
 		local static_fx = ShaderFX(Mod.staticBulletShader, {
 			["time"] = function() return Kristal.getTime() end,
-			["brightness"] = 0.2
+			["brightness"] = 0.3
 		})
 		self:addFX(static_fx, "static_fx")
 	end
