93de06c0c996d63aa587ec7366265a8a5dc39146
name = theLucidLibrarian <alexanderhaejunkim@gmail.com>
2026-07-13 19:55:29 -0700

    a

--- scripts/world/maps/secrets/secret_sanctuary/map.lua
+++ scripts/world/maps/secrets/secret_sanctuary/map.lua
@@ -11,24 +11,42 @@ function map:onEnter()
 	if flag == true then
 		Game.world.map:getEvent(70):remove()
 	end
-	local angles = {math.rad(0), math.rad(45), math.rad(90), math.rad(135), math.rad(180), math.rad(225), math.rad(270), math.rad(315)}
-	for _, event in ipairs(self.events) do
-		if event.layer == self.layers["objects_parallax"] then
-			event:setOrigin(0.5, 0.5)
-			event.x = math.random(100, 400)
-			event.y = math.random(150, 200)
-			event.rotation = TableUtils.pick(angles)
+	local rand = love.math.random(1,100)
+		if rand == 5 then	
+			Game:setFlag("funni_tower_shenanigans", true)
 		end
-	end
+	local angles = {math.rad(45), math.rad(90), math.rad(135), math.rad(180), math.rad(225), math.rad(270), math.rad(315), math.rad(360)}
+	for _, event in ipairs(self.events) do
+			if event.layer == self.layers["objects_parallax"] then
+				event:setOrigin(0.5, 0.5)
+				event.x = math.random(80, 520)
+				event.y = math.random(120, 240)
+				event.rotation = TableUtils.pick(angles)
+			end
+		end
 end
 
 function map:update()
     super.update(self)
-
+	local angles = {math.rad(45), math.rad(90), math.rad(135), math.rad(180), math.rad(225), math.rad(270), math.rad(315), math.rad(360)}
+	self.siner = self.siner + DTMULT
+	if Game:getFlag("funni_tower_shenanigans") == true then
+		for _, event in ipairs(self.events) do
+			if event.layer == self.layers["objects_parallax"] then
+				event:setOrigin(0.5, 0.5)
+				event.x = math.random(80, 520)
+				event.y = math.random(120, 240)
+				event.rotation = TableUtils.pick(angles)
+			end
+		end
+	end
 end
 
 function map:onExit()
 	Game:setFlag("passed_savepoint", true)
+	if Game:getFlag("funni_tower_shenanigans") == true then
+		Game:setFlag("funni_tower_shenanigans", false)
+	end
 end
 
 return map
